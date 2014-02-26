ef705b46879812bc3d10f1d89fe2fe60f52f3182
“hcm90” <heitor.moura@gmail.com>
2014-02-26 12:22:19 -0500

    Finished up some association work

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,14 @@
 class User < ActiveRecord::Base
-	has_many :projects, :through :pledges => "Contributor"
-	has_many :projects, :class_name => "Owner"
+	
+	has_many :projects, :foreign_key => "owner_id"
+	#has_many :owned_projects, :class_name => "Project"
+
+
+	has_many :contributed_projects, :through => :pledges, :source => :project
+
+
+
+
+	#has_many :projects, :through :pledges, :class_name => "Contributor"
+	#has_many :projects, :class_name => "Owner"
 end
