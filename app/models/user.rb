924aa74428ec969c1bc032670ece785aa98ce513
“hcm90” <heitor.moura@gmail.com>
2014-02-26 15:31:55 -0500

    Fixed migration for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,5 @@
 class User < ActiveRecord::Base
-	
-	has_many :projects, :foreign_key => "owner_id"
-	#has_many :owned_projects, :class_name => "Project"
-
-
-	has_many :contributed_projects, :through => :pledges, :source => :project
-
-
-
-
-	#has_many :projects, :through :pledges, :class_name => "Contributor"
-	#has_many :projects, :class_name => "Owner"
+  authenticates_with_sorcery!
+  has_many :projects, :foreign_key => "owner_id"
+  has_many :contributed_projects, :through => :pledges, :source => :project
 end
